43356140725d059e370cb316e0af4a59b6ce3192
Leon <hualiang.tian@gmail.com>
2021-07-23 14:30:07 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,15 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def create
+    post = Post.create(post_params)
+    redirect_to post
+  end 
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name])
+  end
 end
